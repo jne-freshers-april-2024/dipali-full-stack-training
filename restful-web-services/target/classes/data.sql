1cb8f87196d34ef5d7b908da675348452973b480
dipali.patil@thinkitive.com <dipali.patil@thinkitive.com>
2024-04-15 20:40:54 +0530

    react-1

---
+++ restful-web-services/target/classes/data.sql
@@ -0,0 +1,8 @@
+insert into user_details(birth_date,id,name) values(current_date(),10001,'Jayashree');
+insert into user_details(birth_date,id,name) values(current_date(),10002,'Mayuri');
+insert into user_details(birth_date,id,name) values(current_date(),10003,'Shivani');
+
+insert into post(id,user_id,description) values (20001,10001,'Learning JavaSript');
+insert into post(id,user_id,description) values (20002,10001,'Learning HTML&CSS');
+insert into post(id,user_id,description) values (20003,10002,'Learning SpringBoot');
+insert into post(id,user_id,description) values (20004,10002,'Learning React');
